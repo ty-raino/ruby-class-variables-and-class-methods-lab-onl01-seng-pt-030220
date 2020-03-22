e5aaac2cea8471ec4bb80f1973d1a7d278ab9868
Ty Raino <mstymetris@yahoo.com>
2020-03-22 04:00:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,12 @@ class Song
        genre_count[genre] = 1
      end
 
+  def self.artist_count
+    artist_count = {}
+     artist_count.each do |artist|
+    if artist_count[artist]
+         artist_count += 1
+      else
+       artist_count[artist] = 1
+    end
 end
